bd3df3f5db3f88f33e26ba7faedff713b66db9fe
mecolli2 <michael.e.collins92@gmail.com>
2017-07-25 15:02:28 -0500

    Create ruth_2.js
    Create ruth_2.js

---
+++ jquery_files/bible_app/ruth_2.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	
+	$('.r2-3-4v').click(function(e){
+		$('#r2-3-4p').slideToggle(500);
+		e.preventDefault();
+	});
+	
+	$('.r2-11-12v').click(function(e){
+		$('#r2-11-12p').slideToggle(500);
+		e.preventDefault();
+	});
+	
+	$('.r2-20v').click(function(e){
+		$('#r2-20p').slideToggle(500);
+		e.preventDefault();
+	});
+	
+});
