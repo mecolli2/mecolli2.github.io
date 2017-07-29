f7ffc194fc36606a683f570c6700596a3dfb5ee3
mecolli2 <michael.e.collins92@gmail.com>
2017-07-29 14:05:43 -0500

    Update ruth_1.js
    Minor bug fixes

--- jquery_files/bible_app/ruth_1.js
+++ jquery_files/bible_app/ruth_1.js
@@ -2,11 +2,14 @@ $(document).ready(function(){
 	
 	$('.r1-1-5v').click(function(e){
 		$('#r1-1-5p').slideToggle(500);
+		$('footer').slideToggle(500);
+		$('.thisnext').toggleClass('higher');
 		e.preventDefault();
 	});
 	
 	$('.r1-22v').click(function(e){
 		$('#r1-22p').slideToggle(500);
+		$('footer').slideToggle(500);
 		e.preventDefault();
 	});
 	
