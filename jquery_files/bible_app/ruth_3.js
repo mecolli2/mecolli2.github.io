9427d145a44b6d238e9de0f9c8f4946145781fbe
mecolli2 <michael.e.collins92@gmail.com>
2018-05-21 20:11:31 -0500

    Create ruth_3.js

---
+++ jquery_files/bible_app/ruth_3.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	
+	$('.r3-2v').click(function(e){
+		$('#r3-2p').slideToggle(500);
+		e.preventDefault();
+	});
+	
+	$('.r3-7v').click(function(e){
+		$('#r3-7p').slideToggle(500);
+		e.preventDefault();
+	});
+	
+	$('.r3-9v').click(function(e){
+		$('#r3-9p').slideToggle(500);
+		e.preventDefault();
+	});
+	
+	$('.r3-10v').click(function(e){
+		$('#r3-10p').slideToggle(500);
+		e.preventDefault();
+	});
+
+});
